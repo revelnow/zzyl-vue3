be09b284aa649ba6caebcbcced8b794141a8b144
theo <3142905784@qq.com>
2026-03-22 02:21:08 +0800

    护理计划功能开发

--- src/api/nursing/project.js
+++ src/api/nursing/project.js
@@ -42,3 +42,10 @@ export function delProject(id) {
     method: 'delete'
   })
 }
+//查询所有护理项目
+export function getAllProjects() {
+  return request({
+    url: '/nursing/project/all',
+    method: 'get'
+  })
+}
--- src/views/nursing/project/index.vue
+++ src/views/nursing/project/index.vue
@@ -9,11 +9,6 @@
           @keyup.enter="handleQuery"
         />
       </el-form-item>
-<<<<<<< HEAD
-      <el-form-item>
-        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
-        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
-=======
       <el-form-item label="状态" prop="status" >
         <el-select v-model="queryParams.status" placeholder="请选择" clearable style="width: 100px">
           <el-option v-for="item in nursing_project_status" :key="item.value" :label="item.label" :value="item.value" />
@@ -24,7 +19,6 @@
       <el-form-item>
          <el-button icon="Refresh" @click="resetQuery">重置</el-button>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
->>>>>>> 7376303 (修改页面细节)
       </el-form-item>
     </el-form>
 
@@ -38,50 +32,12 @@
           v-hasPermi="['nursing:project:add']"
         >新增</el-button>
       </el-col>
-<<<<<<< HEAD
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['nursing:project:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['nursing:project:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['nursing:project:export']"
-        >导出</el-button>
-      </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
-    </el-row>
-
-    <el-table v-loading="loading" :data="projectList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="编号" align="center" prop="id" />
-=======
      
     </el-row>
 
     <el-table v-loading="loading" :data="projectList" @selection-change="handleSelectionChange">
       <!-- <el-table-column label="编号" align="center" prop="id" /> -->
         <el-table-column label="序号" type="index" align="center" width="60"/>
->>>>>>> 7376303 (修改页面细节)
       <el-table-column label="名称" align="center" prop="name" />
       <el-table-column label="排序号" align="center" prop="orderNo" />
       <el-table-column label="单位" align="center" prop="unit" />
@@ -92,13 +48,6 @@
         </template>
       </el-table-column>
       <el-table-column label="护理要求" align="center" prop="nursingRequirement" />
-<<<<<<< HEAD
-      <el-table-column label="状态" align="center" prop="status" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-        <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['nursing:project:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['nursing:project:remove']">删除</el-button>
-=======
         <el-table-column label="创建时间" align="center" prop="createTime" width="180">
          <template #default="scope">
            <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
@@ -106,8 +55,8 @@
       </el-table-column>
       <el-table-column label="状态" align="center" prop="status" >
         <template #default="scope">
-          <el-tag :type="scope.row.status === 0 ? 'success' : 'danger'">
-            {{ scope.row.status === 0 ? '启用' : '禁用' }}
+          <el-tag :type="scope.row.status === 1 ? 'success' : 'danger'">
+            {{ scope.row.status === 1 ? '启用' : '禁用' }}
           </el-tag>
         </template>
       </el-table-column>
@@ -115,10 +64,9 @@
         <template #default="scope" >
           <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['nursing:project:edit']">修改</el-button>
           <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['nursing:project:remove']">删除</el-button>
-          <el-button link type="primary" :icon="scope.row.status === 0 ? 'lock' : 'unlock'" @click="handleStatus(scope.row)">
-            {{ scope.row.status === 0 ? '禁用' : '启用' }}
+          <el-button link type="primary" :icon="scope.row.status === 1 ? 'lock' : 'unlock'" @click="handleStatus(scope.row)">
+            {{ scope.row.status === 0 ? '启用' : '禁用' }}
           </el-button>
->>>>>>> 7376303 (修改页面细节)
         </template>
       </el-table-column>
     </el-table>
@@ -138,15 +86,6 @@
           <el-input v-model="form.name" placeholder="请输入名称" />
         </el-form-item>
         <el-form-item label="排序号" prop="orderNo">
-<<<<<<< HEAD
-          <el-input v-model="form.orderNo" placeholder="请输入排序号" />
-        </el-form-item>
-        <el-form-item label="单位" prop="unit">
-          <el-input v-model="form.unit" placeholder="请输入单位" />
-        </el-form-item>
-        <el-form-item label="价格" prop="price">
-          <el-input v-model="form.price" placeholder="请输入价格" />
-=======
             <el-input-number v-model="form.orderNo" 
               placeholder="请输入" 
               :min="1" 
@@ -161,17 +100,11 @@
               :min="5" 
               :max="100" 
               :step="5"  controls-position="right"/>
->>>>>>> 7376303 (修改页面细节)
         </el-form-item>
         <el-form-item label="图片" prop="image">
           <image-upload v-model="form.image"/>
         </el-form-item>
         <el-form-item label="护理要求" prop="nursingRequirement">
-<<<<<<< HEAD
-          <el-input v-model="form.nursingRequirement" placeholder="请输入护理要求" />
-        </el-form-item>
-      </el-form>
-=======
           <el-radio-group v-model="form.status">
         <el-radio v-for="dict in nursing_project_status" 
         :key="dict.value" 
@@ -183,7 +116,6 @@
       <el-input v-model="form.nursingRequirement" placeholder="请输入护理要求" type="textarea"/>
     </el-form-item>
    </el-form>
->>>>>>> 7376303 (修改页面细节)
       <template #footer>
         <div class="dialog-footer">
           <el-button type="primary" @click="submitForm">确 定</el-button>
@@ -197,14 +129,10 @@
 <script setup name="Project">
 import { listProject, getProject, delProject, addProject, updateProject } from "@/api/nursing/project"
 
-<<<<<<< HEAD
-const { proxy } = getCurrentInstance()
-=======
 // 获取当前实例
 const { proxy } = getCurrentInstance()
 //引用数据字典
 const { nursing_project_status } = proxy.useDict("nursing_project_status");
->>>>>>> 7376303 (修改页面细节)
 
 const projectList = ref([])
 const open = ref(false)
@@ -278,11 +206,8 @@ function reset() {
   proxy.resetForm("projectRef")
 }
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 7376303 (修改页面细节)
 /** 搜索按钮操作 */
 function handleQuery() {
   queryParams.value.pageNum = 1
@@ -315,10 +240,7 @@ function handleUpdate(row) {
   const _id = row.id || ids.value
   getProject(_id).then(response => {
     form.value = response.data
-<<<<<<< HEAD
-=======
     form.value.status = response.data.status.toString() // 将状态转换为字符串以适应单选框
->>>>>>> 7376303 (修改页面细节)
     open.value = true
     title.value = "修改护理项目"
   })
@@ -356,15 +278,6 @@ function handleDelete(row) {
   }).catch(() => {})
 }
 
-<<<<<<< HEAD
-/** 导出按钮操作 */
-function handleExport() {
-  proxy.download('nursing/project/export', {
-    ...queryParams.value
-  }, `project_${new Date().getTime()}.xlsx`)
-}
-
-=======
 /** 状态按钮操作 */
 function handleStatus(row) {
   const newStatus = row.status === 0 ? 1 : 0  ;
@@ -378,6 +291,5 @@ function handleStatus(row) {
 }
 
 
->>>>>>> 7376303 (修改页面细节)
 getList()
 </script>
